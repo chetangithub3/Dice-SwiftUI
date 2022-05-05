252091a68f97b57e47772fe4cd3311ba6b2099b9
Chetan <chetan.getsmail@gmail.com>
2022-05-04 20:32:09 -0400

    Initial Commit

---
+++ Dice-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dice-SwiftUI
+//
+//  Created by Chetan Dhowlaghar on 5/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
